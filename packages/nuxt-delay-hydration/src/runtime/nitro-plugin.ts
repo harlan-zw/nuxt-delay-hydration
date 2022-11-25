c2e18c3604c2a7988d543a6b3c01f857f16f51e3
Harlan Wilton <harlan@harlanzw.com>
2022-11-25 16:52:04 +1100

    fix: resolve filter properly

--- packages/nuxt-delay-hydration/src/runtime/nitro-plugin.ts
+++ packages/nuxt-delay-hydration/src/runtime/nitro-plugin.ts
@@ -1,12 +1,49 @@
 import type { NitroAppPlugin } from 'nitropack'
 import { packString } from 'packrup'
 import { MODE_DELAY_APP_INIT } from '../module'
-import { createFilter } from '../util/filter'
+import { createRouter, toRouteMatcher } from 'radix3'
 import { useRuntimeConfig } from '#imports'
 
 import { debug, exclude, include, mode, replayScript, script } from '#delay-hydration'
 
 const SCRIPT_REGEX = /<script(.*?)>/gm
+
+
+export interface CreateFilterOptions {
+  include?: (string | RegExp)[]
+  exclude?: (string | RegExp)[]
+  strictTrailingSlash?: boolean
+}
+
+export function createFilter(options: CreateFilterOptions = {}): (path: string) => boolean {
+  const include = options.include || []
+  const exclude = options.exclude || []
+
+  return function (path: string): boolean {
+    for (const v of [{ rules: exclude, result: false }, { rules: include, result: true }]) {
+      const regexRules = v.rules.filter(r => r instanceof RegExp) as RegExp[]
+      if (regexRules.some(r => r.test(path)))
+        return v.result
+
+      const stringRules = v.rules.filter(r => typeof r === 'string') as string[]
+      if (stringRules.length > 0) {
+        const routes = {}
+        for (const r of stringRules) {
+          // quick scan of literal string matches
+          if (r === path)
+            return v.result
+
+          // need to flip the array data for radix3 format, true value is arbitrary
+          routes[r] = true
+        }
+        const routeRulesMatcher = toRouteMatcher(createRouter({ routes, ...options }))
+        if (routeRulesMatcher.matchAll(path).length > 0)
+          return Boolean(v.result)
+      }
+    }
+    return include.length === 0
+  }
+}
 
 export default <NitroAppPlugin> function (nitro) {
   nitro.hooks.hook('render:html', (htmlContext, { event }) => {

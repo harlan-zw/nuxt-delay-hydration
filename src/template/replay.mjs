e0e6723e336767f8b19f0dfc5f1348e3322f730f
Saurav B V <41701284+sauravbv@users.noreply.github.com>
2024-09-06 20:39:19 +0530

    fix: replayClick script (#57)

--- src/template/replay.mjs
+++ src/template/replay.mjs
@@ -1,7 +1,7 @@
 /* eslint-disable */
 (() => {
   w._$delayHydration.then((e) => {
-    if (e instanceof PointerEvent || (e instanceof MouseEvent && e.type !== 'click') || (window.TouchEvent && e instanceof TouchEvent)) {
+    if (e instanceof PointerEvent || (e instanceof MouseEvent && e.type === 'click') || (window.TouchEvent && e instanceof TouchEvent)) {
       setTimeout(() => {
         return w.requestIdleCallback(() => setTimeout(() => e.target && e.target.click(), 500));
       }, 50);

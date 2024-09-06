c49201273752bac6a24754cb9a387f96e73e3775
Saurav B V <41701284+sauravbv@users.noreply.github.com>
2024-09-06 16:41:13 +0530

    fix: replayClick script

--- src/runtime/template/replay.mjs
+++ src/runtime/template/replay.mjs
@@ -1,1 +1,1 @@
-(()=>{w._$delayHydration.then(e=>{if(e instanceof PointerEvent||e instanceof MouseEvent&&e.type!=="click"||window.TouchEvent&&e instanceof TouchEvent){setTimeout(()=>w.requestIdleCallback(()=>setTimeout(()=>e.target&&e.target.click(),500)),50)}})})();
+(()=>{w._$delayHydration.then(e=>{if(e instanceof PointerEvent||e instanceof MouseEvent&&e.type==="click"||window.TouchEvent&&e instanceof TouchEvent){setTimeout(()=>w.requestIdleCallback(()=>setTimeout(()=>e.target&&e.target.click(),500)),50)}})})();
